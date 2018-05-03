219d63dd7942e1775b5af627108ef28aca57e12c
Karol Tatała <ktatala@wikia-inc.com>
2018-05-03 23:34:39 +0200

    fix Group controller

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ssh latata@t2.latata.pl 'rm -rf /home/latata/t2/backend'
 scp -r $TRAVIS_BUILD_DIR/build latata@t2.latata.pl:/home/latata/t2/
-tar cf - -C $TRAVIS_BUILD_DIR backend | ssh latata@t2.latata.pl "cd /home/latata/t2 && tar xvf - && cd backend && npm install && sudo pm2 reload server"
+tar cf - -C $TRAVIS_BUILD_DIR backend | ssh latata@t2.latata.pl "cd /home/latata/t2 && tar xf - && cd backend && npm install && sudo pm2 reload server"
